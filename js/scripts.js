3ffc1ab00a8a1efddb97847652602da453b5d1a8
kiarie <denniskiarie426@gmail.com>
2021-02-09 10:47:42 +0300

    used scripts.js to make page interactive

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,47 @@
 $(document).ready(function(){
-    $()
+
+    $("#confirm").click(function(){
+        $("#paragraph").show()
+    })
+
+    $("#answer2").click(function(){
+        $("#paragraph2").show()
+    })
+
+    $("#answer3").click(function(){
+        $("#paragraph3").show()
+    })
+
+    $("#answer4").click(function(){
+        $("#paragraph4").show()
+    })
+
+    $("#answer5").click(function(){
+        $("#paragraph5").show()
+    })
+
+    $("#answer6").click(function(){
+        $("#paragraph6").show()
+    })
+
+    $("#answer7").click(function(){
+        $("#paragraph7").show()
+    })
+
+    $("#answer8").click(function(){
+        $("#paragraph8").show()
+    })
+
+    $("#answer9").click(function(){
+        $("#paragraph9").show()
+    })
+
+    $("#answer10").click(function(){
+        $("#paragraph10").show()
+    })
+
+    $("#answer11").click(function(){
+        $("#paragraph11").show()
+    })
+    
 })
